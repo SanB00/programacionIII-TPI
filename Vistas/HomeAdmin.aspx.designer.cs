8093b025eb0450de6775170cc2b8a07519679f24
SantiB <151892199+SanB00@users.noreply.github.com>
2026-06-16 23:58:11 -0300

    de Elian Gaston Maspero - martes, 16 de junio de 2026, 01:55 Buenas, Continuando con el trabajo , decidi agregar la parte del home admin con los linkButtons correspondientes, Agrande el tamaño del texto de bienvenida al admin y  con el atributo Style modifique un poco el diseño de forma simple para que se vean un poco mas prolijos los linkButtons en la tabla
    Adjunto imagen_2026-06-16_014851487.png
     TPINT_GRUPO_18_PR3 v6.rar

--- Vistas/HomeAdmin.aspx.designer.cs
+++ Vistas/HomeAdmin.aspx.designer.cs
@@ -22,5 +22,41 @@ namespace Vistas
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblBienvenida;
+
+        /// <summary>
+        /// Control lbMedicos_admin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbMedicos_admin;
+
+        /// <summary>
+        /// Control lbTurnos_admin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbTurnos_admin;
+
+        /// <summary>
+        /// Control lbPacientes_admin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbPacientes_admin;
+
+        /// <summary>
+        /// Control lbInformes_admin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbInformes_admin;
     }
 }
